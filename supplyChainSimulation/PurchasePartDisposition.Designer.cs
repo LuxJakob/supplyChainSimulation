0d0acde037f9ad9d46d0f60599e6fe6210bb746a
LuxJakob <112105354+LuxJakob@users.noreply.github.com>
2024-11-19 13:41:52 +0100

    Update PurchasePartDisposition.Designer.cs

--- supplyChainSimulation/PurchasePartDisposition.Designer.cs
+++ supplyChainSimulation/PurchasePartDisposition.Designer.cs
@@ -31,13 +31,32 @@
             switchCapacityPlanning = new Button();
             switchBuildXML = new Button();
             tableLayoutPanel1 = new TableLayoutPanel();
+            label18 = new Label();
+            label17 = new Label();
+            label16 = new Label();
+            label14 = new Label();
+            label13 = new Label();
+            label12 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label38 = new Label();
+            label15 = new Label();
+            label11 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
             label1 = new Label();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // switchCapacityPlanning
             // 
             switchCapacityPlanning.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            switchCapacityPlanning.Location = new Point(12, 918);
+            switchCapacityPlanning.Location = new Point(12, 963);
             switchCapacityPlanning.Name = "switchCapacityPlanning";
             switchCapacityPlanning.Size = new Size(75, 23);
             switchCapacityPlanning.TabIndex = 12;
@@ -48,7 +67,7 @@
             // switchBuildXML
             // 
             switchBuildXML.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            switchBuildXML.Location = new Point(1397, 918);
+            switchBuildXML.Location = new Point(1397, 963);
             switchBuildXML.Name = "switchBuildXML";
             switchBuildXML.Size = new Size(75, 23);
             switchBuildXML.TabIndex = 13;
@@ -58,29 +77,45 @@
             // 
             // tableLayoutPanel1
             // 
-            tableLayoutPanel1.ColumnCount = 17;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnCount = 15;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 91F));
+            tableLayoutPanel1.Controls.Add(label18, 14, 1);
+            tableLayoutPanel1.Controls.Add(label17, 13, 1);
+            tableLayoutPanel1.Controls.Add(label16, 12, 1);
+            tableLayoutPanel1.Controls.Add(label14, 11, 1);
+            tableLayoutPanel1.Controls.Add(label13, 10, 1);
+            tableLayoutPanel1.Controls.Add(label12, 9, 1);
+            tableLayoutPanel1.Controls.Add(label10, 8, 1);
+            tableLayoutPanel1.Controls.Add(label9, 7, 1);
+            tableLayoutPanel1.Controls.Add(label8, 6, 1);
+            tableLayoutPanel1.Controls.Add(label38, 5, 1);
+            tableLayoutPanel1.Controls.Add(label15, 13, 0);
+            tableLayoutPanel1.Controls.Add(label11, 9, 0);
+            tableLayoutPanel1.Controls.Add(label7, 5, 0);
+            tableLayoutPanel1.Controls.Add(label6, 4, 0);
+            tableLayoutPanel1.Controls.Add(label5, 3, 0);
+            tableLayoutPanel1.Controls.Add(label4, 2, 0);
+            tableLayoutPanel1.Controls.Add(label3, 1, 0);
+            tableLayoutPanel1.Controls.Add(label2, 0, 0);
             tableLayoutPanel1.Location = new Point(12, 37);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 29;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
@@ -111,6 +146,212 @@
             tableLayoutPanel1.Size = new Size(1370, 880);
             tableLayoutPanel1.TabIndex = 14;
             // 
+            // label18
+            // 
+            label18.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label18.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label18.Location = new Point(1277, 30);
+            label18.Name = "label18";
+            label18.Size = new Size(90, 30);
+            label18.TabIndex = 141;
+            label18.Text = "Mode";
+            label18.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label17
+            // 
+            label17.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label17.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label17.Location = new Point(1186, 30);
+            label17.Name = "label17";
+            label17.Size = new Size(85, 30);
+            label17.TabIndex = 140;
+            label17.Text = "Quantity";
+            label17.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label16
+            // 
+            label16.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label16.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label16.Location = new Point(1095, 30);
+            label16.Name = "label16";
+            label16.Size = new Size(85, 30);
+            label16.TabIndex = 139;
+            label16.Text = "X";
+            label16.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            label14.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label14.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label14.Location = new Point(1004, 30);
+            label14.Name = "label14";
+            label14.Size = new Size(85, 30);
+            label14.TabIndex = 138;
+            label14.Text = "X";
+            label14.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            label13.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label13.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label13.Location = new Point(913, 30);
+            label13.Name = "label13";
+            label13.Size = new Size(85, 30);
+            label13.TabIndex = 137;
+            label13.Text = "X";
+            label13.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            label12.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.Location = new Point(822, 30);
+            label12.Name = "label12";
+            label12.Size = new Size(85, 30);
+            label12.TabIndex = 136;
+            label12.Text = "X";
+            label12.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            label10.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(731, 30);
+            label10.Name = "label10";
+            label10.Size = new Size(85, 30);
+            label10.TabIndex = 135;
+            label10.Text = "X";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            label9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(640, 30);
+            label9.Name = "label9";
+            label9.Size = new Size(85, 30);
+            label9.TabIndex = 134;
+            label9.Text = "X";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(549, 30);
+            label8.Name = "label8";
+            label8.Size = new Size(85, 30);
+            label8.TabIndex = 133;
+            label8.Text = "X";
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label38
+            // 
+            label38.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label38.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label38.Location = new Point(458, 30);
+            label38.Name = "label38";
+            label38.Size = new Size(85, 30);
+            label38.TabIndex = 132;
+            label38.Text = "X";
+            label38.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label15
+            // 
+            label15.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            tableLayoutPanel1.SetColumnSpan(label15, 2);
+            label15.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label15.Location = new Point(1186, 0);
+            label15.Name = "label15";
+            label15.Size = new Size(181, 30);
+            label15.TabIndex = 102;
+            label15.Text = "Order ";
+            label15.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            label11.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            tableLayoutPanel1.SetColumnSpan(label11, 4);
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(822, 0);
+            label11.Name = "label11";
+            label11.Size = new Size(358, 30);
+            label11.TabIndex = 98;
+            label11.Text = "Inventory After Period X";
+            label11.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            tableLayoutPanel1.SetColumnSpan(label7, 4);
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(458, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(358, 30);
+            label7.TabIndex = 94;
+            label7.Text = "Requirement According To Production Program";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(367, 0);
+            label6.Name = "label6";
+            tableLayoutPanel1.SetRowSpan(label6, 2);
+            label6.Size = new Size(85, 60);
+            label6.TabIndex = 93;
+            label6.Text = "Current Inventory";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(276, 0);
+            label5.Name = "label5";
+            tableLayoutPanel1.SetRowSpan(label5, 2);
+            label5.Size = new Size(85, 60);
+            label5.TabIndex = 92;
+            label5.Text = "Discount Quantity";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(185, 0);
+            label4.Name = "label4";
+            tableLayoutPanel1.SetRowSpan(label4, 2);
+            label4.Size = new Size(85, 60);
+            label4.TabIndex = 91;
+            label4.Text = "Standard Deviation";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(94, 0);
+            label3.Name = "label3";
+            tableLayoutPanel1.SetRowSpan(label3, 2);
+            label3.Size = new Size(85, 60);
+            label3.TabIndex = 90;
+            label3.Text = "Delivery Period";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            tableLayoutPanel1.SetRowSpan(label2, 2);
+            label2.Size = new Size(85, 60);
+            label2.TabIndex = 89;
+            label2.Text = "Purchased Part";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
             // label1
             // 
             label1.AutoSize = true;
@@ -125,13 +366,14 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1484, 953);
+            ClientSize = new Size(1484, 998);
             Controls.Add(label1);
             Controls.Add(tableLayoutPanel1);
             Controls.Add(switchBuildXML);
             Controls.Add(switchCapacityPlanning);
             Name = "PurchasePartDisposition";
             Text = "Supply Chain Simulator - Purchase Part Disposition";
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -142,5 +384,23 @@
         private Button switchBuildXML;
         private TableLayoutPanel tableLayoutPanel1;
         private Label label1;
+        private Label label15;
+        private Label label11;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label18;
+        private Label label17;
+        private Label label16;
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label38;
     }
 }
